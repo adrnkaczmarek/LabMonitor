f5f77204fa2bae4e6267aa1dc51f21c098b7e260
KrzysztofR-PUT <krzysztof.rozga@student.put.poznan.pl>
2016-04-12 17:15:52 +0200

    Poprawa mastera

--- src/client/Main.java
+++ src/client/Main.java
@@ -5,8 +5,5 @@ public class Main {
     public static void main(String[] args) {
     	ScreenViewClient streamScreen = new ScreenViewClient("localhost", 11937);
     	streamScreen.startTimer();
-//        ProcessListClient streamProcess = new ProcessListClient();
-//        streamProcess.connectAndSend("localhost", 11938);
-
     }    
 }
