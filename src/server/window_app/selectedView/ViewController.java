d9375d1103e76f5cc88d83d884b7b09833201bd2
KrzysztofR-PUT <krzysztof.rozga@student.put.poznan.pl>
2016-04-13 22:41:33 +0200

    Dodany GridPane w SelectedView, oraz wyswietlanie procesow

--- src/server/window_app/selectedView/ViewController.java
+++ src/server/window_app/selectedView/ViewController.java
@@ -1,17 +1,30 @@
 package server.window_app.selectedView;
 
+import javafx.application.Platform;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.GridPane;
 import processLib.ProcessModel;
+import processLib.ProcessParse;
+import processLib.ProcessServer;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+
+//Dobra mamy tutaj GridPane'a, Adus ma pierwsza kolumne, Krzychu i Kuba druga podzielona na dwa wiersze
+//Jak cos ta lista procesow poki co to troche lipa, bo jak bedzie uruchomionych kilka klientow to bedzie co chwile zmienial ta tabele, musze dodac komunikacje zeby jeden tylko wysylal
 
 public class ViewController implements Initializable{
 
@@ -20,11 +33,27 @@ public class ViewController implements Initializable{
 
     private TableView table = new TableView();
     private ObservableList<ProcessModel> data;
+    private Boolean isProcessServerOn = false;
+
+
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        //=================DO WYWALENIA TESTY GRIDA==============
+        Label testAdus = new Label("Miejsce dla Marka");
+        Label testKuba = new Label("Miejsce dla Kuby");
+        gridPane.add(testAdus, 0, 0);
+        gridPane.add(testKuba, 1, 0);
+        //=======================================================
+
+
         setupProcessTable();
     }
+
+
+
+
 
     private void setupProcessTable() {
         table.setEditable(true);
@@ -36,11 +65,48 @@ public class ViewController implements Initializable{
         sessionName.setCellValueFactory(new PropertyValueFactory<ProcessModel, String>("sessionName"));
 
         table.getColumns().addAll(processName, sessionName);
-        table.setPadding(new Insets(0, 0, 0, 0));
+        table.setPadding(new Insets(10, 10, 10, 10));
         table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
 
         table.setItems(data);
 
-        gridPane.add(table, 1, 0);
+        gridPane.add(table, 1, 1);
+        listenForProcesses();
+    }
+
+    private void listenForProcesses() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    runProcessServer();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private void runProcessServer() throws ExecutionException, InterruptedException {
+        isProcessServerOn = true;
+        while (isProcessServerOn == true) {
+            ExecutorService executorService = Executors.newSingleThreadExecutor();
+            Future<List<String>> future = executorService.submit(new ProcessServer(6066));
+
+            ProcessParse dataParser = new ProcessParse(future.get());
+            data = dataParser.getOutputList();
+
+            Platform.runLater(() -> {
+                table.setItems(data);
+            });
+
+            executorService.shutdown();
+        }
+    }
+
+    private void stopServer() {
+        isProcessServerOn = false;
     }
 }
