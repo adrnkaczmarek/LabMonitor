4b251db62db1eba3337de5906028a698153c58ee
Adrian <adrian_kaczmarek94@wp.pl>
2016-04-26 18:31:25 +0200

    Wyodrębniona klasa SendScreenTask

--- src/client/ScreenViewClient.java
+++ src/client/ScreenViewClient.java
@@ -1,74 +1,22 @@
 package client;
 
-import library.Conversions;
-import library.IOOperations;
-import library.Screen;
-
-import java.awt.*;
-import java.io.DataOutputStream;
 import java.net.Socket;
 import java.util.Timer;
-import java.util.TimerTask;
 
 public class ScreenViewClient {
 	
 	private Timer timer;
-	private String hostName;
-	private int port;
+    private Socket socket;
 	
 	public ScreenViewClient(String name, int portNmb){
-		this.hostName = name;
-		this.port = portNmb;
+        try {
+            socket = new Socket(name, portNmb);
+        }catch( Exception e){e.printStackTrace();}
 	}
-	
-	
 	
 	public void startTimer(){
     	timer = new Timer();
-        SendImageTask task = new SendImageTask( hostName, port );
+        SendScreenTask task = new SendScreenTask( socket, timer, 4 );
         timer.schedule(task, 0, 10);
-    }
-
-    class SendImageTask extends TimerTask{
-    	
-    	private Socket refSock;
-    	private DataOutputStream output;
-        private Dimension screenDimension;
-    	
-    	protected SendImageTask( String name, int port ){
-    		try {
-				this.refSock = new Socket( name, port );
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-    		this.output = IOOperations.initOutput(refSock);
-            screenDimension = Screen.getScreenDimension(4);
-    	}
-
-    	
-    	public DataOutputStream getOutput(){
-    		return this.output;
-    	}
-    	
-		@Override
-		public void run() {
-
-			DataOutputStream outputLocal = null;
-			try{
-				outputLocal = this.getOutput();
-
-				byte[] a = Conversions.imageToByteArray( Screen.getScreenshot(screenDimension) );
-				outputLocal.writeInt(a.length);
-				outputLocal.write(a);
-
-			}catch(Exception e){
-                e.printStackTrace();
-                timer.cancel();
-                try {
-                    outputLocal.close();
-                    refSock.close();
-                } catch (Exception exc) {e.printStackTrace();}
-			}
-        }
     }
 }
---
+++ src/client/SendScreenTask.java
@@ -0,0 +1,52 @@
+package client;
+
+import library.Conversions;
+import library.IOOperations;
+import library.Screen;
+
+import java.awt.*;
+import java.io.DataOutputStream;
+import java.net.Socket;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SendScreenTask extends TimerTask {
+
+    private Socket refSock;
+    private DataOutputStream output;
+    private Dimension screenshotDimension;
+    private Timer timer;
+
+    protected SendScreenTask(Socket socket, Timer timer, int imageSizeDivider){
+        refSock = socket;
+        output = IOOperations.initOutput(refSock);
+        screenshotDimension = Screen.getScreenDimension(imageSizeDivider);
+        this.timer = timer;
+    }
+
+
+    public DataOutputStream getOutput(){
+        return this.output;
+    }
+
+    @Override
+    public void run() {
+
+        DataOutputStream outputLocal = null;
+        try{
+            outputLocal = getOutput();
+
+            byte[] a = Conversions.imageToByteArray(Screen.getScreenshot(screenshotDimension));
+            outputLocal.writeInt(a.length);
+            outputLocal.write(a);
+
+        }catch(Exception e){
+            e.printStackTrace();
+            timer.cancel();
+            try {
+                outputLocal.close();
+                refSock.close();
+            } catch (Exception exc) {e.printStackTrace();}
+        }
+    }
+}
